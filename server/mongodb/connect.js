64cff03104276d538393431c88dd7ae3c669df8c
Kozikov Bohdan <kozikovbohdan@Kozikovs-MacBook-Air.local>
2023-04-24 13:29:22 +0300

    changed client links

---
+++ server/mongodb/connect.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const connectDB = (url) => {
+  mongoose.get('strictQuery', true);
+
+  mongoose.connect(url)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err))
+}
+
+export default connectDB;
